f8f67a8d643a4004601d131097fc8428ba4565fa
renasboy <renasboy@gmail.com>
2012-12-02 00:21:31 +0100

    Added API key, paths and initial algo

--- js/search.js
+++ js/search.js
@@ -1,16 +1,48 @@
-var apiKey = 'FILDTEOIK2HBORODV';
-var url = 'http://developer.echonest.com/api/v4/artist/reviews';
+var apiKey          = 'MXNBGHMQLSOTLCNWW';
+var artistLocation  = 'brazil';
+var mood            = 'happy;'
+var mood_potency    = 2;
+var moods           = [ 'happy', 'angry', 'sad', 'relaxing', 'excited' ];
+var url             = 'http://developer.echonest.com/api/v4/artist/search';
 $.ajax({
 	url: url,
 	dataType: 'json',
 	data: {
 		api_key: apiKey,
-		id: 'ARH6W4X1187B99274F',
+        artist_location: artistLocation,
+        //style: ,
+        // max_familiarity: ,
+        // min_familiarity: ,
+        // max_hotttnesss: ,
+        // min_hotttnesss: ,
+        mood: mood + '^' + mood_potency,
 		format: 'json',
-		results: 1,
+		results: 50,
 		start: 0
 	},
 	success: function(data) {
-		console.debug(data);
+		//console.debug(data);
+        var url = 'http://developer.echonest.com/api/v4/song/search';
+
+        $.ajax({
+            url: url,
+            dataType: 'json',
+            data: {
+                api_key: apiKey,
+                artist_id: data.response.artists[0].id,
+                //style: ,
+                //max_danceability: ,
+                //min_danceability: ,
+                //bucket: 'id:spotify-WW',
+                //bucket: 'tracks',
+                //mood: mood + '^' + mood_potency,
+                format: 'json',
+                results: 50,
+                start: 0
+            },
+            success: function(data) {
+                console.debug(data);
+            }
+        });
 	}
 });
