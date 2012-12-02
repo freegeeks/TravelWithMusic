26e2083087c446a04037dd5f8368cdc85dbea7f1
renasboy <renasboy@gmail.com>
2012-12-02 05:02:54 +0100

    Populated search object with main algo, search on songs and search on artists

--- js/search.js
+++ js/search.js
@@ -6,60 +6,143 @@ var Search = function() {
     // Constructor
 };
 
-Search.prototype.load = function(options, callback) {
-    var artistLocation  = 'netherlands';
-    var mood            = 'happy;'
-    var mood_potency    = 2;
-    var moods           = [ 'happy', 'angry', 'sad', 'relaxing', 'excited' ];
-    var url             = 'http://developer.echonest.com/api/v4/artist/search';
+// Search by artists
+Search.prototype.artists = function (options, callback) {
+    var url = 'http://developer.echonest.com/api/v4/artist/search';
+
+    var data = new Object;
+    data.api_key    = Config.echonest.apiKey;
+    data.results    = 1;
+    data.start      = 0;
+
+    if (options.mood) {
+        data.mood = options.mood;
+    }
+    if (options.artist_location) {
+        data.artist_location = options.artist_location;
+    }
+
     $.ajax({
         url: url,
         dataType: 'json',
-        data: {
-            api_key: Config.echonest.apiKey,
-            artist_location: artistLocation,
-            //style: ,
-            // max_familiarity: ,
-            // min_familiarity: ,
-            // max_hotttnesss: ,
-            // min_hotttnesss: ,
-            mood: mood + '^' + mood_potency,
-            format: 'json',
-            results: 50,
-            start: 0
-        },
+        data: data,
         success: function(data) {
-            //console.debug(data);
-            var url = 'http://developer.echonest.com/api/v4/song/search?bucket=tracks&bucket=id:spotify-WW';
-
-            $.ajax({
-                url: url,
-                dataType: 'json',
-                data: {
-                    api_key: Config.echonest.apiKey,
-                    artist_id: data.response.artists[0].id,
-                    //style: ,
-                    //max_danceability: ,
-                    //min_danceability: ,
-                    //bucket: 'id:spotify-WW',
-                    //bucket: 'tracks',
-                    //mood: mood + '^' + mood_potency,
-                    format: 'json',
-                    results: 50,
-                    start: 0
-                },
-                success: function(data) {
-                    callback(data.response.songs);
-                }
-            });
+            callback(data.response.artists);
         }
     });
 };
 
+// Search by songs
+Search.prototype.songs = function (options, callback) {
+    var url = 'http://developer.echonest.com/api/v4/song/search?bucket=tracks&bucket=id:spotify-WW';
+
+    var data = new Object;
+    data.api_key    = Config.echonest.apiKey;
+    data.format     = 'json';
+    data.results    = 3;
+    data.start      = 0;
+    //data.sort       = 'duration-asc';
+    //data.limit      = true;
+
+    if (options.results) {
+        data.results= options.results;
+    }
+    if (options.description) {
+        data.description= options.description;
+    }
+    if (options.artist_id) {
+        data.artist_id = options.artist_id;
+    }
+    else if (options.mood) {
+        data.mood = options.mood;
+    }
+    if (options.dance) {
+        data.min_danceability = 0;
+        data.max_danceability = 1;
+    }
+
+    $.ajax({
+        url: url,
+        dataType: 'json',
+        data: data,
+        success: function(data) {
+            callback(data.response.songs);
+        }
+    });
+};
+
+// Search for songs based on the full config
+Search.prototype.load = function(options, callback) {
+    var results = [];
+    var that = this;
+    
+    // search by themes
+    if (options.themes) {
+        for (var i in options.themes) {
+            var theme = options.themes[i];
+            this.songs({ description: theme }, function (data) {
+                results = results.concat(data);
+            });
+        }
+    }
+
+    var moods           = [ 'happy', 'angry', 'sad', 'relaxing', 'excited' ];
+
+    // search by location in the map
+    if (options.locations) {
+        var loop    = options.locations.length;
+        var k       = 1;
+        for (var i in options.locations) {
+            var loc     = options.locations[i];
+            this.artists({
+                artist_location: loc.name,
+                mood: loc.mood
+            }, function (data) {
+                for (var j in data) {
+                    that.songs({
+                        artist_id: data[j].id,
+                        mood: loc.mood,
+                        dance: loc.dance,
+                        results: loc.frequency
+                    }, function (data) {
+                        results = results.concat(data);
+                        if (k++ == loop) {
+                            callback(results);
+                        }
+                    });
+                }
+            });
+        }
+    }
+};
 
 var search = new Search();
 search.load({
-    location: 'netherlands'
+    themes: [ 'xmas' ],
+    locations: [
+        {
+            name: 'netherlands',
+            dance: 0,
+            frequency: 1,
+            mood: 'happy'
+        },
+        {
+            name: 'brazil',
+            dance: 0,
+            frequency: 4,
+            mood: 'happy'
+        },
+        {
+            name: 'italy',
+            dance: 0,
+            frequency: 1,
+            mood: 'sad'
+        }
+    ]
 }, function(data) {
-    console.log(data);
+    for (var i in data) {
+        if (data[i].tracks.length && data[i].tracks[0].foreign_id) {
+            console.debug(data[i].tracks[0].foreign_id + ' ' + data[i].artist_name + ' - ' + data[i].title);
+        }
+    }
 });
