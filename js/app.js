8489d5f31aa9731ea3984969cc139509be6972b0
Renan Gonçalves <renan.saddam@gmail.com>
2012-12-02 08:15:30 +0100

    Getting mapping from song => location.

--- js/app.js
+++ js/app.js
@@ -5,7 +5,17 @@ var App = function(models) {
 App.prototype.search = function(latitude, longitude, callback) {
     var self = this;
 
-    this._search(latitude, longitude, function(results) {
+    this._search(latitude, longitude, function(data, mapping) {
+        var results = [],
+            mapping = {};
+        for (var i in data) {
+            if (data[i].tracks.length && data[i].tracks[0].foreign_id) {
+                var cleanId = data[i].tracks[0].foreign_id.replace('spotify-WW', 'spotify');
+                results.push(cleanId);
+                mapping[cleanId] = data[i].location;
+            }
+        }
+
         var premise = self.models.Playlist.createTemporary('Travel with Music Playlist');
         premise.done(function(playlist) {
             playlist._collections();
@@ -20,12 +30,11 @@ App.prototype.search = function(latitude, longitude, callback) {
                     'name', 'playable'
                 ]).done(function(track) {
                     if (track.playable) {
-                        console.log(track.name);
                         playlist.tracks.add(track);
                     }
                     
                     if (++tries == total) {
-                        callback(playlist);
+                        callback(playlist, mapping);
                     }
                 });
             }
@@ -38,7 +47,6 @@ App.prototype._search = function(latitude, longitude, callback) {
 
     var search = new Search();
     search.locationByGeo(latitude, longitude, function(location) {
-
         search.weatherByLocation(location, function(weather) {
             search.load({
                 themes: [ ],
@@ -54,26 +62,17 @@ App.prototype._search = function(latitude, longitude, callback) {
                         mood: 'happy'
                     }
                 ]
-            }, function(data) {
-                console.log(data);
-                var results = [];
-                for (var i in data) {
-                    if (data[i].tracks.length && data[i].tracks[0].foreign_id) {
-                        var cleanId = data[i].tracks[0].foreign_id.replace('spotify-WW', 'spotify');
-                        results.push(cleanId);
-                    }
-                }
-                callback(results);
-            });
-
+            }, callback);
         });
     });
 };
 
 require(['$api/models'], function(models) {
     window.App = new App(models);
-    window.App.search(52.37, 4.89, function(playlist) {
+    window.App.search(52.37, 4.89, function(playlist, mapping) {
         models.player.setShuffle(true);
         models.player.playContext(playlist);
+
+        console.log(mapping);
     });
 });
